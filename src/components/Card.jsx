e207752f0b875fe8d0b93980eddfa25755261552
Elton Rexha <elton.rexha123@outlook.com>
2024-09-11 18:29:57 +0200

    Add style for card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,22 @@ import PropTypes from 'prop-types';
 
 export default function Card({ name, imgUrl, price, category }) {
   return (
-    <div>
-      <img src={imgUrl} alt={category + ' ' + name}></img>
-      <p>{name}</p>
-      <p>{price}</p>
-      <p>{category}</p>
+    <div className="p-2 min-w-64 h-80 rounded opacity-95 hover:opacity-100 transition ease-in-out delay-100 bg-gray-100 shadow-sm shadow-slate-200 cursor-pointer">
+      <img
+        className="w-[100%] mb-4 h-48 object-cover rounded"
+        src={imgUrl}
+        alt={category + ' ' + name}
+      ></img>
+      <p className="font-bold font-mono text-xl">{price}€</p>
+      <p className="text-gray-700">{category}</p>
+      <p className="font-medium">{name}</p>
     </div>
   );
 }
 
 Card.propTypes = {
-    name: PropTypes.string,
-    imgUrl: PropTypes.string,
-    price: PropTypes.number,
-    category: PropTypes.number
-}
+  name: PropTypes.string,
+  imgUrl: PropTypes.string,
+  price: PropTypes.number,
+  category: PropTypes.number,
+};
