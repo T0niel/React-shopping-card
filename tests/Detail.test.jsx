d516ab4fa2548dc3a7e07ec352b95d1c0b77c056
Elton Rexha <elton.rexha123@outlook.com>
2024-09-12 19:18:56 +0200

    use children on detail test instead

--- tests/Detail.test.jsx
+++ tests/Detail.test.jsx
@@ -5,34 +5,38 @@ import userEvent from '@testing-library/user-event';
 
 describe('Detail', () => {
   it('Match snapshot', () => {
-    const { container } = render(<Detail text={''} description={''} />);
+    const { container } = render(<Detail text={''} ></Detail>);
     expect(container).toMatchSnapshot();
   });
 
   it('Displays the text', () => {
-    render(<Detail text={'Hello'} description={''} />);
+    render(<Detail text={'Hello'} ></Detail>);
 
     const text = screen.queryByText('hello');
 
     expect(text).not.toEqual(null);
   });
 
-  it('Displays description onclick', () => {
+  it('Displays children onclick', () => {
     const user = userEvent.setup();
 
     const { container } = render(
-      <Detail text={'test'} description={'some description'} />
+      <Detail text={'test'} >
+        <h1>some description</h1>
+      </Detail>
     );
 
     user.click(container);
     expect(screen.queryByText('some description')).not.toEqual(null);
   });
 
-  it('Hides description when clicked twice', () => {
+  it('Hides children when clicked twice', () => {
     const user = userEvent.setup();
 
     const { container } = render(
-      <Detail text={'test'} description={'some description'} />
+      <Detail text={'test'}>
+        <h1>some description</h1>
+      </Detail>
     );
 
     user.click(container);
@@ -41,8 +45,12 @@ describe('Detail', () => {
     expect(screen.queryByText('some description')).toEqual(null);
   });
 
-  it('Does not display description when not clicked', () => {
-    render(<Detail text={'test'} description={'some description'} />);
+  it('Does not display children when not clicked', () => {
+    render(
+      <Detail text={'test'}>
+        <h1>some description</h1>
+      </Detail>
+    );
 
     expect(screen.queryByText('some description')).toEqual(null);
   });
