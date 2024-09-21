22b56419011ed7a63f9fce57d39fe1b047dce012
Elton Rexha <elton.rexha123@outlook.com>
2024-09-21 23:37:35 +0200

    add style to the ImageCarousel, and cache mechanism

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,31 +1,73 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export default function ImageCarousel({ imgs }) {
   const [index, setIndex] = useState(0);
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    imgs.forEach((img) => {
+      const image = new Image();
+      image.src = img.src; // Image starts loading and gets cached
+    });
+  }, [imgs]);
+
+  useEffect(() => {
+    setLoading(true);
+  }, [index]);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
+
+  const handlePrevClick = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + imgs.length) % imgs.length);
+  };
+
+  const handleNextClick = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % imgs.length);
+  };
 
   if (imgs.length === 0) return <div>No images available</div>;
 
   return (
-    <div>
-      <img src={imgs[index].src} alt={imgs[index].alt}></img>
-      <button
-        onClick={() => {
-          setIndex((index + 1) % imgs.length);
-        }}
-      >
-        {'>'}
-      </button>
-      <button
-        onClick={() => {
-          setIndex((index - 1 + imgs.length) % imgs.length);
-        }}
-      >
-        {'<'}
-      </button>
+    <div className="w-[100%] h-[100%] relative border-2 rounded border-gray-100">
+      {loading && (
+        <div className="loader absolute top-0 left-0 right-0 bottom-0 -z-10 flex justify-center items-center m-[auto]">
+        </div>
+      )}
+
+      <img
+        src={imgs[index].src}
+        alt={imgs[index].alt}
+        className={`object-contain w-[100%] h-[100%] ${
+          loading ? 'opacity-50' : 'opacity-100'
+        }`} 
+        onLoad={handleImageLoad}
+      />
+      {imgs.length > 1 && (
+        <>
+          <button
+            className="absolute top-0 h-[100%] w-8 font-bold text-xl"
+            onClick={handlePrevClick}
+          >
+            <div className="w-[100%] h-[100%] bg-gray-400 rounded hover:opacity-40 transition-opacity ease-in delay-100 opacity-20 absolute top-0"></div>
+            {'<'}
+          </button>
+          <button
+            className="absolute top-0 right-0 h-[100%] w-8 font-bold text-xl"
+            onClick={handleNextClick}
+          >
+            <div className="w-[100%] h-[100%] bg-gray-400 rounded hover:opacity-40 transition-opacity ease-in delay-100 opacity-20 absolute top-0"></div>
+            {'>'}
+          </button>
+        </>
+      )}
     </div>
   );
 }
+
+
 
 
 ImageCarousel.propTypes = {
